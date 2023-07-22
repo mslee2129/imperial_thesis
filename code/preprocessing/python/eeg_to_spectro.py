596555b6f757cc4657f34f89d5b0aa1bd3ca19f3
Jason Lee <mslee2129@gmail.com>
2023-07-22 10:03:47 +0100

    data mod

--- code/preprocessing/python/eeg_to_spectro.py
+++ code/preprocessing/python/eeg_to_spectro.py
@@ -14,10 +14,11 @@ def read_mat(filename):
     mat = sio.loadmat(dir+filename)
     return mat
 
-n = 1
+
 for fname in os.listdir(dir):
     if pathlib.Path(fname).suffix == '.mat':
         mat = read_mat(fname)
+        n = fname[5]
         key = list(mat.keys())[3]
         eeg = mat[key]
         sfreq = 125
@@ -25,10 +26,10 @@ for fname in os.listdir(dir):
             print('stim' + str(n) + '_sub' + str(i))
             sub = eeg[:,:,i]
             for j in range(0, sub.shape[1], 125):
-                if n < 10:
-                    name = dst + 'stim0' + str(n) + '_sub' + str(i) + '_' + str(int(j/125)) + '.tiff'
+                if int(n) > 0:
+                    name = dst + 'stim0' + n + '_sub' + str(i) + '_' + str(int(j/125)) + '.tiff'
                 else:
-                    name = dst + 'stim' + str(n) + '_sub' + str(i) + '_' + str(int(j/125)) + '.tiff'
+                    name = dst + 'stim10' + '_sub' + str(i) + '_' + str(int(j/125)) + '.tiff'
 
                 sample = sub[:,j:j+125]
                 psd, freqs = mne.time_frequency.psd_array_welch(sample, sfreq, fmin=1, fmax=63.5, n_fft=256, n_per_seg=256, verbose=False)
@@ -37,4 +38,3 @@ for fname in os.listdir(dir):
                 norm = matplotlib.colors.Normalize()
                 im = Image.fromarray(np.uint8(cmap(norm(psd))*255))
                 im.save(name)
-        n += 1
