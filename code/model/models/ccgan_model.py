297c69ae9b6c5978b31db0a71c61e0d8d0b90913
Min Sang Lee <jl5922@ic.ac.uk>
2023-07-31 17:18:05 +0100

    cycle implemented;

--- code/model/models/ccgan_model.py
+++ code/model/models/ccgan_model.py
@@ -3,6 +3,7 @@ import itertools
 from util.image_pool import ImagePool
 from .base_model import BaseModel
 from . import model
+from torch.autograd import Variable
 
 class CCGANModel(BaseModel):
     '''
@@ -22,8 +23,8 @@ class CCGANModel(BaseModel):
     def __init__(self, opt):
         BaseModel.__init__(self, opt)
 
-        self.loss_names = ['G_A', 'G_B', 'D_A', 'D_B', 'cycle_A', 'idt_A']
-        self.visual_names_A = ['real_A', 'fake_B', 'real_B']
+        self.loss_names = ['G_A', 'G_B', 'D_A', 'D_B', 'cycle_A', 'cycle_B', 'idt_A', 'idt_B']
+        self.visual_names_A = ['real_A', 'fake_B', 'rec_A']
         self.visual_names_B = ['real_B', 'fake_A', 'rec_B']
 
         if self.isTrain and self.opt.lambda_identity > 0.0:
@@ -33,7 +34,7 @@ class CCGANModel(BaseModel):
         self.visual_names = self.visual_names_A + self.visual_names_B
 
         if self.isTrain:
-            self.model_names = ['G_A', 'G_B', 'D_A', 'D_B']
+            self.model_names = ['G_A', 'G_B', 'D_A', 'D_B', 'D_C', 'D_D']
         else:
             self.model_names = ['G_A', 'G_B']
 
@@ -44,14 +45,16 @@ class CCGANModel(BaseModel):
                                      not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
         
         if self.isTrain:
-            self.netD_A = model.define_D(opt.input_nc + opt.output_nc, opt.ndf, opt.netD, opt.n_layers_D,
+            self.netD_A = model.define_D(opt.output_nc, opt.ndf, opt.netD, opt.n_layers_D,
                                          opt.norm, opt.init_type, opt.init_gain, self.gpu_ids)
             self.netD_B = model.define_D(opt.input_nc, opt.ndf, opt.netD, opt.n_layers_D, 
                                          opt.norm, opt.init_type, opt.init_gain, self.gpu_ids)
+            self.netD_C = model.define_D(opt.input_nc + opt.output_nc, opt.ndf, opt.netD, opt.n_layers_D,
+                                         opt.norm, opt.init_type, opt.init_gain, self.gpu_ids)
+            self.netD_D = model.define_D(opt.input_nc + opt.output_nc, opt.ndf, opt.netD, opt.n_layers_D,
+                                         opt.norm, opt.init_type, opt.init_gain, self.gpu_ids)
             
         if self.isTrain:
-            if opt.lambda_identity > 0.0:
-                assert(opt.input_nc == opt.output_nc)
             self.fake_A_pool = ImagePool(opt.pool_size)
             self.fake_B_pool = ImagePool(opt.pool_size)
             # define loss functions
@@ -60,9 +63,15 @@ class CCGANModel(BaseModel):
             self.criterionIdt = torch.nn.L1Loss()
             # initialise optimisers
             self.optimizer_G = torch.optim.Adam(itertools.chain(self.netG_A.parameters(), self.netG_B.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
-            self.optimizer_D = torch.optim.Adam(itertools.chain(self.netD_A.parameters(), self.netD_B.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizer_D_A = torch.optim.Adam(itertools.chain(self.netD_A.parameters(), self.netD_B.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizer_D_B = torch.optim.Adam(itertools.chain(self.netD_B.parameters(), self.netD_B.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizer_D_C = torch.optim.Adam(itertools.chain(self.netD_C.parameters(), self.netD_B.parameters()), lr=0.0001, betas=(opt.beta1, 0.999))
+            self.optimizer_D_D = torch.optim.Adam(itertools.chain(self.netD_C.parameters(), self.netD_B.parameters()), lr=0.0001, betas=(opt.beta1, 0.999))
             self.optimizers.append(self.optimizer_G)
-            self.optimizers.append(self.optimizer_D)
+            self.optimizers.append(self.optimizer_D_A)
+            self.optimizers.append(self.optimizer_D_B)
+            self.optimizers.append(self.optimizer_D_C)
+            self.optimizers.append(self.optimizer_D_D)
 
     def set_input(self, input):
         AtoB = self.opt.direction == 'AtoB'
@@ -72,8 +81,10 @@ class CCGANModel(BaseModel):
 
     def forward(self):
         self.fake_B = self.netG_A(self.real_A) # G_A(A)
+        self.rec_A = self.netG_B(self.fake_B)   # G_B(G_A(A))
         self.fake_A = self.netG_B(self.real_B) # G_B(B)
         self.rec_B = self.netG_A(self.fake_A) # G_A(G_B(B))
+
 
     def backward_D_basic(self, netD, real, fake):
         '''
@@ -100,35 +111,46 @@ class CCGANModel(BaseModel):
         fake_A = self.fake_A_pool.query(self.fake_A)
         self.loss_D_B = self.backward_D_basic(self.netD_B, self.real_A, fake_A)
 
-    def backward_G_A(self):
-        """Calculate GAN and L1 loss for generator A"""
-        # First, G(A) should fake the discriminator
-        fake_AB = torch.cat((self.real_A, self.fake_B), 1)
-        pred_fake = self.netD_A(fake_AB)
-        self.loss_G_GAN = self.criterionGAN(pred_fake, True)
-        # Second, G(A) = B
-        self.loss_G_L1 = self.criterionCycle(self.fake_B, self.real_B) * self.opt.lambda_L1
-        # combine loss and calculate gradients
-        self.loss_G_A = self.loss_G_GAN + self.loss_G_L1
-        self.loss_G_A.backward()
+    def backward_D_C(self,opt):
+        if opt.super_start==1:
+           loss_D_C = self.backward_D_basic(self.netD_C, torch.cat((self.real_A,(self.real_B)),1), torch.cat((Variable(self.fake_A),(self.real_B)),1))
+           self.loss_D_C = loss_D_C.data[0]
 
-    def backward_G_B(self):
-        """Calculate the loss for generator and G_B"""
+    def backward_D_D(self,opt):
+        if opt.super_start==1:
+           loss_D_D = self.backward_D_basic(self.netD_D, torch.cat((self.real_B,(self.real_A)),1),torch.cat((Variable(self.fake_B),(self.real_A)),1) )
+           self.loss_D_D = loss_D_D.data[0]
+
+    def backward_G(self):
+        """Calculate the loss for generators"""
         lambda_idt = self.opt.lambda_identity
+        lambda_A = self.opt.lambda_A
         lambda_B = self.opt.lambda_B
         # Identity loss
         if lambda_idt > 0:
             # G_A should be identity if real_B is fed: ||G_A(B) - B||
             self.idt_A = self.netG_A(self.real_B)
             self.loss_idt_A = self.criterionIdt(self.idt_A, self.real_B) * lambda_B * lambda_idt
+            # G_B should be identity if real_A is fed: ||G_B(A) - A||
+            self.idt_B = self.netG_B(self.real_A)
+            self.loss_idt_B = self.criterionIdt(self.idt_B, self.real_A) * lambda_A * lambda_idt
         else:
             self.loss_idt_A = 0
+            self.loss_idt_B = 0
        
+        # GAN loss D_A(G_A(A))
+        self.loss_G_A = self.criterionGAN(self.netD_A(self.fake_B), True)
         # GAN loss D_B(G_B(B))
         self.loss_G_B = self.criterionGAN(self.netD_B(self.fake_A), True)
+        # Forward cycle loss || G_B(G_A(A)) - A||
+        self.loss_cycle_A1 = self.criterionGAN(self.netD_D(torch.cat((self.fake_B, self.real_A),1)), True)#*lambda_A*lambda_idt
+        self.loss_cycle_B1 = self.criterionGAN(self.netD_C(torch.cat((self.fake_A, self.real_B),1)), True)#*lambda_B*lambda_idt
+
+        self.loss_cycle_A = self.criterionCycle(self.rec_A, self.real_A) * lambda_A
+        # Backward cycle loss || G_A(G_B(B)) - B||
         self.loss_cycle_B = self.criterionCycle(self.rec_B, self.real_B) * lambda_B
         # combined loss and calculate gradients
-        self.loss_G_B = self.loss_G_B + self.loss_cycle_B + self.loss_idt_A
+        self.loss_G = self.loss_G_A + self.loss_G_B + self.loss_cycle_A + self.loss_cycle_B + self.loss_cycle_A1 + self.loss_cycle_B1 + self.loss_idt_A + self.loss_idt_B
         self.loss_G_B.backward()
 
     def optimize_parameters(self):
@@ -136,14 +158,25 @@ class CCGANModel(BaseModel):
         # forward
         self.forward()      # compute fake images and reconstruction images.
         # G_A and G_B
-        self.set_requires_grad([self.netD_A, self.netD_B], False)  # Ds require no gradients when optimizing Gs
+        self.set_requires_grad([self.netD_A, self.netD_B, self.netD_C, self.netD_D], False)  # Ds require no gradients when optimizing Gs
         self.optimizer_G.zero_grad()  # set G_A and G_B's gradients to zero
-        self.backward_G_A()             # calculate gradients for G_A and G_B
-        self.backward_G_B()   
+        self.backward_G()             # calculate gradients for G_A and G_B
         self.optimizer_G.step()       # update G_A and G_B's weights
-        # D_A and D_B
+        # Discriminators
         self.set_requires_grad([self.netD_A, self.netD_B], True)
-        self.optimizer_D.zero_grad()   # set D_A and D_B's gradients to zero
-        self.backward_D_A()      # calculate gradients for D_A
-        self.backward_D_B()      # calculate graidents for D_B
-        self.optimizer_D.step()  # update D_A and D_B's weights
+        # D_A
+        self.optimizer_D_A.zero_grad()
+        self.backward_D_A()
+        self.optimizer_D_A.step()
+        # D_B
+        self.optimizer_D_B.zero_grad()
+        self.backward_D_B()
+        self.optimizer_D_B.step()
+        # D_C
+        self.optimizer_D_C.zero_grad()
+        self.backward_D_C()
+        self.optimizer_D_C.step()
+        # D_D
+        self.optimizer_D_D.zero_grad()
+        self.backward_D_D()
+        self.optimizer_D_D.step() # update D_A and D_B's weights
--- code/model/train.py
+++ code/model/train.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     opt = TrainOptions().parse()   # get training options
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
     dataset_size = len(dataset)    # get the number of images in the dataset.
-    print('The number of training images = %d' % dataset_size)
+    print('The number of training images for A to B = %d' % dataset_size)
 
     model = create_model(opt)      # create a model given opt.model and other options
     model.setup(opt)               # regular setup: load and print networks; create schedulers
@@ -51,10 +51,10 @@ if __name__ == '__main__':
             model.set_input(data)         # unpack data from dataset and apply preprocessing
             model.optimize_parameters()   # calculate loss functions, get gradients, update network weights
 
-            if total_iters % opt.display_freq == 0:   # display images on visdom and save images to a HTML file
-                save_result = total_iters % opt.update_html_freq == 0
-                model.compute_visuals()
-                visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
+            # if total_iters % opt.display_freq == 0:   # display images on visdom and save images to a HTML file
+            #     save_result = total_iters % opt.update_html_freq == 0
+            #     model.compute_visuals()
+            #     visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
 
             if total_iters % opt.print_freq == 0:    # print training losses and save logging information to the disk
                 losses = model.get_current_losses()
